2d559e134a17e62b94dad5a626709b8b2639011e
pinukond <pinukond@DIN73002729.corp.capgemini.com>
2018-08-06 10:38:58 +0530

    my first commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class MainClass {
+	public static void main(String []args) {
+		System.out.println("hi bro!!");
+		greet();
+	}
+	public static void greet() {
+		System.out.println("he");
+	}
+}
